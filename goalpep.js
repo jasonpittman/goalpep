89f81ddfcf3203ab10a94f68948de71aff38924d
Brian St. Amand <brianstamand@gmail.com>
2011-04-23 01:01:51 -0700

    new dummy proj

--- goalpep.js
+++ goalpep.js
@@ -7,7 +7,14 @@ var projects = [{
 	name:"Eat right",
 	date: new Date('12/4/2003'),
 	mr:100,
-	mt:1000}]
+	mt:1000
+	},{
+	name:"Learn javascript",
+	date: new Date("05/05/1987"),
+	mr: 60,
+	mt: 34
+	}
+	]
 
 module.exports = projects;
 
