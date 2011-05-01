3fb985568d5decb300d8f4a183d0b5f82d88c735
Jason Pittman <ashenhaste@yahoo.com>
2011-05-01 14:42:37 -0500

    Tree prototype 0.04

--- static/javascript/prototype_tree.js
+++ static/javascript/prototype_tree.js
@@ -1,70 +1,32 @@
-/* SimpleTreeMenu */
-
-(function($) {
-
-	var methods = {
+$(document).ready(function(event) {
+  $('#goalPepTree ul')
+    .hide()
+    .prev('span')
+    .before('<span></span>')
+    .prev()
+    .addClass('handle closed')
+    .click(function(){
+      // plus/minus handle click
+      $(this)
+        .toggleClass('closed opened')
+        .nextAll('ul')
+        .toggle();
+		})
 		
-		init: function() {
-			var $this = $(this);
-	    	return this.each(function() {
-	    		if ($this.hasClass("simpleTreeMenu")) {
-	    			// Already done...
-	    		}
-	    		else {
-					$this.hide();
-					$(this).addClass("simpleTreeMenu");
-					$this.children("li").each(function() {
-						methods.buildNode($(this));
-					});	
-					$(this).show();	
-				}
-	    	});		
-		},
-		
-		buildNode: function($li) {
-			if ($li.children("ul").length > 0) {
-				$li.children("ul").hide();
-				$li.addClass("Node").click(function(event) {
-					var $t = $(this);
-					if ($t.hasClass("expanded")) {
-						$t.removeClass("expanded");
-						$t.children("ul").hide();
-						$('#panel').removeClass("selected");
-						$('#panel').html('')
-					} else {
-						$t.addClass("expanded");
-						$t.children("ul").show();
-						$('#panel').addClass("selected");
-						$('#panel').html('Info Goes Here')
-					}
-					event.stopPropagation();
-				});    
-				$li.children("ul").children("li").each(function() {
-					methods.buildNode($(this));
-				});
-			} else {
-				$li.addClass("Leaf").click(function(event) {
-					event.stopPropagation();
-				});
-				return;
-			}		
-		}
-		
-	};
+   $('#goalPepTree a').click(function(event) {
+		//event.stopPropagation();
+		event.preventDefault();
+		$('#panel').html('Info Goes Here');
+	})
+	.hover(
+	  function () {
+		// console.log('hover');
+	    $(this).addClass('highlight');
+	  }, 
+	  function () {
+		// console.log('hover');
+	    $(this).removeClass('highlight');
+	  })
 	
-	$.fn.simpleTreeMenu = function(method) {
-	    if (methods[method]) {
-			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
-	    } else if (typeof method === 'object' || !method) {
-			return methods.init.apply(this, arguments);
-	    } else {
-			$.error('Method ' +  method + ' does not exist on jQuery.simpleTreeMenu');
-	    }    	
-	};
 	
-})(jQuery);
-
-
-			
-			
-	
+});
