aa12fda2b49cb41e96ce6e953d34b89115979a68
Jason Pittman <ashenhaste@yahoo.com>
2011-05-06 11:17:33 -0500

    tree prototype 0.04

--- static/javascript/prototype_tree.js
+++ static/javascript/prototype_tree.js
@@ -1,32 +1,45 @@
-$(document).ready(function(event) {
-  $('#goalPepTree ul')
-    .hide()
-    .prev('span')
-    .before('<span></span>')
-    .prev()
-    .addClass('handle closed')
-    .click(function(){
-      // plus/minus handle click
-      $(this)
-        .toggleClass('closed opened')
-        .nextAll('ul')
-        .toggle();
-		})
-		
-   $('#goalPepTree a').click(function(event) {
-		//event.stopPropagation();
-		event.preventDefault();
-		$('#panel').html('Info Goes Here');
+$(function () {
+ 	$('#goalPepTree a').addClass('nohighlight').append("&#160;").prepend("&#160;");
+ 	$('#panel-content a').addClass('nohighlight').append("&#160;").prepend("&#160;");
+	
+	$('#goalPepTree ul')
+		.hide()
+		.prev('span')
+		.before('<span></span>')
+		.prev()
+		.addClass('handle closed')
+		.click(function(){
+		  // plus/minus handle click
+		  $(this)
+		    .toggleClass('closed opened')
+		    .nextAll('ul')
+		    .toggle();
 	})
-	.hover(
+		
+	$('#goalPepTree a').click(function(event) {
+		//event.stopPropagation();
+		//event.preventDefault();
+		var name = $(this).html();
+
+		$('#panel-content a').html(name);
+		$('#panel-content a').removeClass('highlight').addClass('nohighlight');
+		
+		return false;
+	});
+
+	$('#goalPepTree a').hover(
 	  function () {
-		// console.log('hover');
-	    $(this).addClass('highlight');
+	    $(this).removeClass('nohighlight').addClass('highlight');
 	  }, 
 	  function () {
-		// console.log('hover');
-	    $(this).removeClass('highlight');
-	  })
+	    $(this).removeClass('highlight').addClass('nohighlight');
+	  }
+	);
 	
-	
+	$('#panel-content a').click(function(event) {
+		//event.stopPropagation();
+		//event.preventDefault();
+		return false;
+	});
+
 });
